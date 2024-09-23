e7a2249bc4c828b114a885dc0e33b1f0d5e4d891
Madhuri <madhuribs111@gmail.com>
2024-09-23 23:17:06 +0530

    updated constants

--- src/constants.js
+++ src/constants.js
@@ -7,13 +7,12 @@ export const dialogueData = {
   "cs-degree": `I'm pursuing Computer Science and Engineering, currently in my final year with a CGPA of 8.12 (till 6th sem). Reserved a spot on the wall for my degree.`,
   "sofa-table": `That's my sofa. I like to relax here and practice DSA, and stay active on socials. Check out my <a href= "https://x.com/madhuri_bs111" target="_blank">Twitter</a> and <a href="https://www.linkedin.com/in/madhuribs/" target="_blank">LinkedIn</a>`,
   tv: `That's my TV. I watch a lot of tech youtubers to stay updated in the tech field, learnt from the best like :
-   <a href="https://www.youtube.com/@techwithtim" target="_blank">techwithtim</a>, <a href="https://www.youtube.com/@WebDevSimplified" target="_blank">Web Dev Simplified</a>,
-  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
+   <a href="https://www.youtube.com/@techwithtim" target="_blank">techwithtim</a>, <a href="https://www.youtube.com/@WebDevSimplified" target="_blank">Web Dev Simplified</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1dILQtJ4BxiqaFhThv0alkio-0QfedKva/view?usp=sharing" target="_blank">Check it out?</a>
   Contact me at madhuribs111@gmail.com if you have any interesting job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  personal_projects: `1. This portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. 2. Javascript based <a href="https://github.com/madhuribs111/slot-machine" target="_blank">slot machine</a> 3.<a href="https://github.com/madhuribs111/face-recognition" target="_blank">Face recognition system</a> and <a href = "https://github.com/madhuribs111/hand-gesture-presentation" target="_blank"> Hand gesture presentation</a> using Python.
+  `,
   library: `There are a lot of programming books and computer science notes on my shelves. I like writing and making notes of whatever I learn.`,
   exit: `If you want to exit Madhuri's portfolio, just close the tab.`,
 };
