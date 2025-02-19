6d3010e5b96f47908d24338f907435519342b434
Madhuri <149250958+madhuribs111@users.noreply.github.com>
2025-02-19 19:59:05 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -9,7 +9,7 @@ export const dialogueData = {
   tv: `That's my TV. I watch a lot of tech youtubers to stay updated in the tech field, learnt from the best like :
    <a href="https://www.youtube.com/@techwithtim" target="_blank">techwithtim</a>, <a href="https://www.youtube.com/@WebDevSimplified" target="_blank">Web Dev Simplified</a>!`,
   bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1dILQtJ4BxiqaFhThv0alkio-0QfedKva/view?usp=sharing" target="_blank">Check it out?</a>
+  resume: `This is my desk and on it is my resume. <a href="https://drive.google.com/file/d/1AHvnXTgzXJ8iU5GZd8cyYZMM9bd4Yc34/view?usp=sharing" target="_blank">Check it out?</a>
   Contact me at madhuribs111@gmail.com if you have any interesting job opportunities!`,
   personal_projects: `1. This portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript. 2. Javascript based <a href="https://github.com/madhuribs111/slot-machine" target="_blank">slot machine</a> 3.<a href="https://github.com/madhuribs111/face-recognition" target="_blank">Face recognition system</a> and <a href = "https://github.com/madhuribs111/hand-gesture-presentation" target="_blank"> Hand gesture presentation</a> using Python.
   `,
